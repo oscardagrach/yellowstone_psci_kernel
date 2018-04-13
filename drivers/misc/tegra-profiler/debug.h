114887f94c30db36e11821d04780815d464e6d76
Gagan Grover <ggrover@nvidia.com>
2018-04-13 15:06:33 +0530

    misc: tegra-profiler: prevent speculative load related leak
    Data can be speculatively loaded from memory and stay in cache even
    when bound check fails. This can lead to unintended information
    disclosure via side-channel analysis.
    
    To mitigate this problem, insert speculation barrier.
    
    Bug 1975134
    CVE-2017-5753
    
    Change-Id: I1962110cab6161a54d5432533abbc57236682965
    Signed-off-by: Gagan Grover <ggrover@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1694597
    Reviewed-by: Abhiroop Kaginalkar <akaginalkar@nvidia.com>
    Tested-by: Abhiroop Kaginalkar <akaginalkar@nvidia.com>
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/misc/tegra-profiler/debug.h
+++ drivers/misc/tegra-profiler/debug.h
@@ -1,7 +1,7 @@
 /*
  * drivers/misc/tegra-profiler/debug.h
  *
- * Copyright (c) 2013-2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2013-2018, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -82,7 +82,12 @@ quadd_get_hw_event_str(int event)
 		[QUADD_EVENT_HW_L2_DCACHE_WRITE_MISSES]	= "l2_d_write",
 		[QUADD_EVENT_HW_L2_ICACHE_MISSES]	= "l2_i",
 	};
-	return (event < QM_ARRAY_SIZE(str)) ? str[event] : "invalid event";
+
+	if (event >= QM_ARRAY_SIZE(str))
+		return "invalid event";
+
+	speculation_barrier();
+	return str[event];
 }
 
 #endif	/* __QUADD_DEBUG_H */
